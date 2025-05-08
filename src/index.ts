70ed5d6fc71caea9d365031d55cb02105cd15689
unknown <rashedulislam.edge@gmail.com>
2025-05-08 18:21:06 +0600

    Final version

--- src/index.ts
+++ src/index.ts
@@ -89,7 +89,7 @@ enum Day {
 }
 
 function getDayType(day: Day): string {
-  if (day == Day.Saturday || day == Day.Sunday) {
+  if (day === Day.Saturday || day === Day.Sunday) {
     return "Weekend";
   } else {
     return "Weekday";
