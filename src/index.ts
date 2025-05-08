365c726d250d25c9a75505ffe0cbc3ab2385eaf4
unknown <rashedulislam.edge@gmail.com>
2025-05-08 18:15:15 +0600

    Final version

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,11 @@ function filterByRating(
 }
 
 function concatenateArrays<T>(...arrays: T[][]): T[] {
-  // return arrays.flat(); //This require ES2019 that's why I use the below one's
-  return ([] as T[]).concat(...arrays);
+  const result: T[] = [];
+  for (const array of arrays) {
+    result.push(...array);
+  }
+  return result;
 }
 
 class Vehicle {
